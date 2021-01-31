b0eb98b7df0ebcc1f59c6e03c81970a66eab28a0
AmirAliShokri <amirali.shokri@gmail.com>
2021-01-31 23:11:51 +0330

    Fix 1 Game 2

---
+++ src/Game2/main.js
@@ -0,0 +1,9 @@
+const answerArray = [
+  "It Will Work",
+  "Maybe maybe not",
+  "Probably Not",
+  "I Dont Now",
+];
+const message = document.querySelector(".message");
+const question = document.querySelector("input");
+const ask = document.querySelector("button");
