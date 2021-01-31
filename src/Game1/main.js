ba2710b6578f3d3aa38cbac74098b5e2ae7ec997
AmirAliShokri <amirali.shokri@gmail.com>
2021-01-31 22:13:24 +0330

    Fix 2 Game 1

--- src/Game1/main.js
+++ src/Game1/main.js
@@ -1,8 +1,27 @@
+const coinArray = ["Heads", "Tails"];
+let score = [0, 0];
 const message = document.querySelector(".message");
 const button = document.querySelectorAll("button");
 for (let index = 0; index < button.length; index++) {
   button[index].addEventListener("click", tossCoin);
 }
 function tossCoin(e) {
-  console.log(e.target.innerText);
+  let playerGuess = e.target.innerText;
+  let computerToss = Math.floor(Math.random() * 2);
+  let computerGuess = coinArray[computerToss];
+  message.innerHTML = "Computer Selected : " + computerGuess;
+  let output;
+  if (playerGuess === computerGuess) {
+    output = "Player Win.";
+    score[0]++;
+  } else {
+    output = "Computer Win.";
+    score[1]++;
+  }
+  message.innerHTML =
+    output +
+    "</br> Player Score : " +
+    score[0] +
+    " Computer Score : " +
+    score[1];
 }
