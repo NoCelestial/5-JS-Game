cbb9f3df645a7e05c0c012a6dd3da39802c6f123
AmirAliShokri <amirali.shokri@gmail.com>
2021-02-01 21:12:09 +0330

    Fix 1 Game 3

---
+++ src/Game3/main.js
@@ -0,0 +1,26 @@
+const gameArea = document.querySelector(".game");
+const button = document.querySelector("button");
+let gamePlay = false;
+maker();
+button.addEventListener("click", function () {
+  if (!gamePlay) {
+    gamePlay = true;
+    button.innerHTML = "Check";
+  } else {
+  }
+});
+
+function maker() {
+  for (let index = 0; index < 6; index++) {
+    let el = document.createElement("input");
+    el.setAttribute("type", "number");
+    el.value = 0;
+    el.max = 9;
+    el.min = 0;
+    el.order = 1;
+    el.size = 1;
+    el.style.width = "50px";
+    el.classList.add("numb");
+    gameArea.appendChild(el);
+  }
+}
